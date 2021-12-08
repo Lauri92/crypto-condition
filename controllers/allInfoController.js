83adb734f6f3bf22dd773354bf2ada5ba49e9ec7
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-12-09 00:42:46 +0200

    Refactored heavily to use only 1 route and modified functions to handle any date range

--- controllers/allInfoController.js
+++ controllers/allInfoController.js
@@ -3,21 +3,30 @@ const axios = require('axios');
 
 const getAll = async (req, res) => {
   try {
-    let startDate = req.query.startdate;
-    let endDate = Number(req.query.enddate) + 3600;
+    const startDate = Number(req.query.startdate);
+    const endDate = Number(req.query.enddate) + 3600;
 
     const daysInTheRequest = (endDate - startDate) / 86400;
+    console.log('days in the request', daysInTheRequest);
+
+    const info = await axios.get(
+        `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${startDate}&to=${endDate}`);
+    const pricesChart = info.data.prices;
+    const volumesChart = info.data.total_volumes;
 
     if (daysInTheRequest < 0) {
       res.status(400).send('Invalid dates!');
-    } else if (daysInTheRequest < 1) {
-      res.status(200).json({message: 'Less than a day'});
-    } else if (daysInTheRequest >= 1 && daysInTheRequest <= 89) {
-      const allInfo = await handle1To90Days(startDate, endDate);
-
+    } else if (daysInTheRequest <= 1) {
+      res.status(400).send('Less than a day');
+    } else if (daysInTheRequest > 1 && daysInTheRequest < 90) {
+      const priceValuesClosestToMidnight = await changeHoursToDays(pricesChart);
+      const volumeValuesClosestToMidnight = await changeHoursToDays(
+          volumesChart);
+      const allInfo = await getAllInfo(priceValuesClosestToMidnight,
+          volumeValuesClosestToMidnight);
       res.status(200).json(allInfo);
     } else {
-      const allInfo = await handleOver90Days(startDate, endDate);
+      const allInfo = await getAllInfo(pricesChart, volumesChart);
       res.status(200).json(allInfo);
     }
   } catch (error) {
@@ -27,21 +36,25 @@ const getAll = async (req, res) => {
 
 };
 
-const handleOver90Days = async (startDate, endDate) => {
+const changeHoursToDays = async (hourlist) => {
+  const valuesClosestToMidnight = [];
+  while (hourlist.length > 0) {
+    const hoursOfDay = hourlist.splice(0, 24).map((hour) => {
+      return hour;
+    });
+    valuesClosestToMidnight.push(hoursOfDay[0]);
+  }
+  return valuesClosestToMidnight;
+};
+
+const getAllInfo = async (pricesChart, volumesChart) => {
 
   try {
-    const info = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${startDate}&to=${endDate}`);
-
-    let pricesChart = info.data.prices;
-    let volumesChart = info.data.total_volumes;
-
     let allInfo = {};
-    allInfo.bearishTrend = await getOver90DaysBearishTrend(pricesChart);
-    allInfo.highestVolume = await getOver90DaysHighestTradingVolume(
+    allInfo.bearishTrend = await getBearishTrend(pricesChart);
+    allInfo.highestVolume = await getTradingVolume(
         volumesChart);
-    allInfo.timeMachine = await getOver90DaysBestDaysToBuyAndSell(pricesChart);
-    console.log(allInfo);
+    allInfo.timeMachine = await getBestBuyAndSellDays(pricesChart);
 
     return allInfo;
   } catch (e) {
@@ -49,14 +62,14 @@ const handleOver90Days = async (startDate, endDate) => {
   }
 };
 
-const getOver90DaysBearishTrend = async (pricesChart) => {
+const getBearishTrend = async (pricesChart) => {
 
-  let currentBearishTrend = {
+  const currentBearishTrend = {
     length: 0,
     startDate: 0,
     endDate: 0,
   };
-  let longestBearishTrend = {
+  const longestBearishTrend = {
     length: 0,
     startDate: 0,
     endDate: 0,
@@ -90,7 +103,7 @@ const getOver90DaysBearishTrend = async (pricesChart) => {
   return longestBearishTrend;
 };
 
-const getOver90DaysHighestTradingVolume = async (volumesChart) => {
+const getTradingVolume = async (volumesChart) => {
   const dayWithHighestVolume = volumesChart.sort((a, b) => {
     return b[1] - a[1];
   })[0];
@@ -101,7 +114,7 @@ const getOver90DaysHighestTradingVolume = async (volumesChart) => {
   };
 };
 
-const getOver90DaysBestDaysToBuyAndSell = async (pricesChart) => {
+const getBestBuyAndSellDays = async (pricesChart) => {
   let priceRisesAtAnyPoint = false;
 
   let previousDate = {
@@ -150,161 +163,6 @@ const getOver90DaysBestDaysToBuyAndSell = async (pricesChart) => {
       },
     };
   }
-};
-
-const handle1To90Days = async (startDate, endDate) => {
-
-  try {
-    const info = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${startDate}&to=${endDate}`);
-
-    let pricesChart = info.data.prices;
-    let volumesChart = info.data.total_volumes;
-    // Handle equally long bearish trends => multiple of same length
-
-    let allInfo = {};
-    allInfo.bearishTrend = await get1To90DaysBearishTrend(pricesChart);
-    allInfo.highestVolume = await get1To90DaysHighestTradingVolume(
-        volumesChart);
-    allInfo.timeMachine = await get1To90DaysBestDaysToBuyAndSell(pricesChart);
-    console.log(allInfo);
-    return allInfo;
-  } catch (e) {
-    console.log(e.message);
-  }
-};
-
-const get1To90DaysBearishTrend = async (pricesChart) => {
-
-  const pricesChartCopy = [...pricesChart];
-
-  let valuesClosestToMidnight = [];
-  while (pricesChartCopy.length > 0) {
-    const hoursOfDay = pricesChartCopy.splice(0, 24).map((hour) => {
-      return hour;
-    });
-    valuesClosestToMidnight.push(hoursOfDay[0]);
-  }
-
-  let currentBearishTrend = {
-    length: 0,
-    startDate: 0,
-    endDate: 0,
-  };
-  let longestBearishTrend = {
-    length: 0,
-    startDate: 0,
-    endDate: 0,
-  };
-  let previousDate = {
-    date: valuesClosestToMidnight[0][0],
-    price: valuesClosestToMidnight[0][1],
-  };
-
-  valuesClosestToMidnight.forEach(dataPoint => {
-    const date = dataPoint[0];
-    const price = dataPoint[1];
-    if (price < previousDate.price) {
-      currentBearishTrend.length++;
-      if (currentBearishTrend.startDate === 0) {
-        currentBearishTrend.startDate = date;
-      }
-      if (currentBearishTrend.length > longestBearishTrend.length) {
-        currentBearishTrend.endDate = date;
-        longestBearishTrend.length = currentBearishTrend.length;
-        longestBearishTrend.startDate = currentBearishTrend.startDate;
-        longestBearishTrend.endDate = currentBearishTrend.endDate;
-      }
-    } else {
-      currentBearishTrend.length = 0;
-      currentBearishTrend.startDate = 0;
-      currentBearishTrend.endDate = 0;
-    }
-    previousDate = {date, price};
-  });
-  return longestBearishTrend;
-
-};
-
-const get1To90DaysHighestTradingVolume = async (volumeChart) => {
-
-  let valuesClosestToMidnight = [];
-  while (volumeChart.length > 0) {
-    const hoursOfDay = volumeChart.splice(0, 24).map((hour) => {
-      return hour;
-    });
-    valuesClosestToMidnight.push(hoursOfDay[0]);
-  }
-
-  const dayWithHighestVolume = valuesClosestToMidnight.sort((a, b) => {
-    return b[1] - a[1];
-  })[0];
-
-  return {
-    date: dayWithHighestVolume[0],
-    volume: dayWithHighestVolume[1],
-  };
-};
-
-const get1To90DaysBestDaysToBuyAndSell = async (pricesChart) => {
-
-  const pricesChartCopy = [...pricesChart];
-  let valuesClosestToMidnight = [];
-  while (pricesChartCopy.length > 0) {
-    const hoursOfDay = pricesChartCopy.splice(0, 24).map((hour) => {
-      return hour;
-    });
-    valuesClosestToMidnight.push(hoursOfDay[0]);
-  }
-
-  let priceRisesAtAnyPoint = false;
-  let previousDate = {
-    date: valuesClosestToMidnight[0][0],
-    price: valuesClosestToMidnight[0][1],
-  };
-
-  valuesClosestToMidnight.forEach((day) => {
-    if (day[1] > previousDate.price) {
-      priceRisesAtAnyPoint = true;
-    }
-    previousDate = {
-      date: day[0],
-      price: day[1],
-    };
-  });
-
-  if (priceRisesAtAnyPoint) {
-    const dayWithHighestPrice = valuesClosestToMidnight.sort((a, b) => {
-      return b[1] - a[1];
-    })[0];
-    const dayWithLowestPrice = valuesClosestToMidnight.sort((a, b) => {
-      return a[1] - b[1];
-    })[0];
-
-    return {
-      bestDayToSell: {
-        date: dayWithHighestPrice[0],
-        price: dayWithHighestPrice[1],
-      },
-      bestDayToBuy: {
-        date: dayWithLowestPrice[0],
-        price: dayWithLowestPrice[1],
-      },
-    };
-  } else {
-
-    return {
-      bestDayToSell: {
-        date: '',
-        price: '',
-      },
-      bestDayToBuy: {
-        date: '',
-        price: '',
-      },
-    };
-  }
-
 };
 
 module.exports = {
--- routes/currencyInfoRoute.js
+++ routes/currencyInfoRoute.js
@@ -1,14 +1,8 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
-const bearishTrendController = require('../controllers/bearishTrendController');
-const tradingVolumeController = require('../controllers/tradingVolumeController');
-const timemachineController = require('../controllers/timemachineController');
 const allInfoController = require('../controllers/allInfoController');
 
-router.route('/bearishtrend').get(bearishTrendController.getBearishTrend);
-router.route('/tradingvolume').get(tradingVolumeController.getTradingVolume);
-router.route('/timemachine').get(timemachineController.getBestDates);
 router.route('/allinfo').get(allInfoController.getAll);
 
 module.exports = router;
