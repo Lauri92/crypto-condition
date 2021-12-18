b9bf3dc32e516d8f799fece1b5bf4a636fe20bb4
Lauri92 <55086903+Lauri92@users.noreply.github.com>
2021-12-18 17:28:09 +0200

    Volumes test

--- controllers/allInfoController.js
+++ controllers/allInfoController.js
@@ -13,8 +13,8 @@ const getAll = async (req, res) => {
         `https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${startDate}&to=${endDate}`);
     const pricesChart = info.data.prices;
     const volumesChart = info.data.total_volumes;
-    console.log('Chart:', pricesChart);
-    console.log('Length:', pricesChart.length);
+    console.log('Chart:', volumesChart);
+    console.log('Length:', volumesChart.length);
     if (daysInTheRequest < 0) {
       res.status(400).send('Invalid dates!');
     } else if (daysInTheRequest <= 1) {
